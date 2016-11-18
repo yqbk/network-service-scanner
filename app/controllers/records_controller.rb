2c5c50387c29bd457224cf3a65170d63cd23f68f
Jakub Syrek <jksyrek@gmail.com>
2016-11-18 17:01:41 +0100

    add new record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,4 +4,20 @@ class RecordsController < ApplicationController
     @records = Record.all
   end
 
+  def create
+    @record = Record.new(record_params)
+
+    if @record.save
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def record_params
+      params.require(:record).permit(:title, :amount, :date)
+    end
+
 end
